5e031c5ac609babdc9748bdd23f540217afd903d
Jhonattan Garcia <jhonattanrgc21@gmail.com>
2023-12-03 19:59:52 -0400

    Prueba de estilos para los botones

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,32 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        success: {
+          50: '#E0F2F1',
+          100: '#B2DFDB',
+          200: '#80CBC4',
+          300: '#4DB6AC',
+          400: '#26A69A',
+          500: '#009688',
+          600: '#00897B',
+          700: '#00796B',
+          800: '#00695C',
+          900: '#004D40',
+        },
+        danger: {
+					50: '#FEF0ED',
+					500: '#B00020'
+				},
+        warning: {
+					50: '#FFAE1F1A',
+					500: '#FFAE1F'
+				} ,
+				light: '#0000001F',
+				dark: '#000000DE'
+      }
+    },
   },
   plugins: [
 		require('@tailwindcss/forms')
