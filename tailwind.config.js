473281987acab6ed85fb673864aa040583867084
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2024-02-12 16:50:55 -0400

    feat: header page styles and structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        success: {
-          50: '#E0F2F1',
-          100: '#B2DFDB',
-          200: '#80CBC4',
-          300: '#4DB6AC',
-          400: '#26A69A',
-          500: '#009688',
-          600: '#00897B',
-          700: '#00796B',
-          800: '#00695C',
-          900: '#004D40',
-        },
-        danger: {
+	content: [
+		"./src/**/*.{html,ts}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				success: {
+					50: '#E0F2F1',
+					100: '#B2DFDB',
+					200: '#80CBC4',
+					300: '#4DB6AC',
+					400: '#26A69A',
+					500: '#009688',
+					600: '#00897B',
+					700: '#00796B',
+					800: '#00695C',
+					900: '#004D40',
+				},
+				danger: {
 					50: '#EABDC5',
 					100: '#DF97A4',
 					200: '#D77D8D',
@@ -27,7 +27,7 @@ module.exports = {
 					500: '#B00020',
 					600: '#98011C',
 				},
-        warning: {
+				warning: {
 					50: '#FFE9C5',
 					100: '#FFDDA4',
 					200: '#FFD58D',
@@ -43,10 +43,26 @@ module.exports = {
 				light: '#0000001F',
 				dark: '#000000DE',
 				disabled: '#00000061'
-      }
-    },
-  },
-  plugins: [
+			}
+		},
+		screens: {
+			'sm': '412px',
+			// => @media (min-width: 640px) { ... }
+
+			'md': '768px',
+			// => @media (min-width: 768px) { ... }
+
+			'lg': '1024px',
+			// => @media (min-width: 1024px) { ... }
+
+			'xl': '1440px',
+			// => @media (min-width: 1280px) { ... }
+
+			'2xl': '1920px',
+			// => @media (min-width: 1536px) { ... }
+		}
+	},
+	plugins: [
 		require('@tailwindcss/forms')
 	],
 }
