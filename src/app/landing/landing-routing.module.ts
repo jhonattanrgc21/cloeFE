b7e1b34fb63a2650ff0aca7216120ac4a7bbe6ec
Jhonattan Garcia <jhonattanrgc21@gmail.com>
2024-02-13 19:15:15 -0400

    Primera seccion de la vista home del landing

--- src/app/landing/landing-routing.module.ts
+++ src/app/landing/landing-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './landing.component';
 import { GatheringCenterComponent } from './pages/gathering-center/gathering-center.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
 	{
@@ -9,8 +10,16 @@ const routes: Routes = [
 		component: LandingComponent,
 		children: [
 			{
+				path: 'home',
+				component: HomeComponent,
+			},
+			{
 				path: 'gathering-center',
 				component: GatheringCenterComponent,
+			},
+			{
+				path: '**',
+				redirectTo: 'home',
 			},
 		],
 	},
--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -5,13 +5,15 @@ import { LandingRoutingModule } from './landing-routing.module';
 import { LandingComponent } from './landing.component';
 import { LandingHeaderComponent } from './components/landing-header/landing-header.component';
 import { LandingFooterComponent } from './components/landing-footer/landing-footer.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
 @NgModule({
   declarations: [
     LandingComponent,
     LandingHeaderComponent,
-    LandingFooterComponent
+    LandingFooterComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule,
