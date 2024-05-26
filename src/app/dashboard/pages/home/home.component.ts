dd89f831da5e142b28201e11cdb14b3fbb897ad3
Jhonattan Garcia <jhonattanrgc21@gmail.com>
2024-05-26 15:08:00 -0400

    Maquete la vista de inicio en el dashboard

--- src/app/dashboard/pages/home/home.component.ts
+++ src/app/dashboard/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+	constructor(private _authService: AuthService){}
 
+	getFullName(){
+		return this._authService.currentFullName;
+	}
 }
