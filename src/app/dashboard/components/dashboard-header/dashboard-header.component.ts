449d8669076189f4ef15101cd5d54920baea30eb
Jhonattan Garcia <jhonattanrgc21@gmail.com>
2024-06-08 17:23:04 -0400

    Modifique el estilo del boton de menu y agregue un pop up de confirmacion al salir del sistema

--- src/app/dashboard/components/dashboard-header/dashboard-header.component.ts
+++ src/app/dashboard/components/dashboard-header/dashboard-header.component.ts
@@ -3,37 +3,37 @@ import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
-  selector: 'app-dashboard-header',
-  templateUrl: './dashboard-header.component.html',
-  styleUrls: ['./dashboard-header.component.scss']
+	selector: 'app-dashboard-header',
+	templateUrl: './dashboard-header.component.html',
+	styleUrls: ['./dashboard-header.component.scss']
 })
 export class DashboardHeaderComponent implements OnInit, OnDestroy {
+	@Output() sidebarToggled = new EventEmitter<void>();
+	private _authSubscription!: Subscription;
 	isUserMenu: boolean = false;
 	fullName: string = '';
 	role: string = '';
 	letter: string = '';
-  @Output() sidebarToggled = new EventEmitter<void>();
-	private _authSubscription!: Subscription;
 
-	constructor(private _authService: AuthService){
-	}
+	constructor(private _authService: AuthService) {}
+
 	ngOnDestroy(): void {
-		if(this._authSubscription) this._authSubscription.unsubscribe();
+		if (this._authSubscription) this._authSubscription.unsubscribe();
 	}
 
 	ngOnInit(): void {
 		this._authSubscription = this._authService.currentUser$.subscribe(user => {
-			this.fullName = user?  `${user.name} ${user.lastname}`: this._authService.currentFullName;
-			this.role = user? user.role: this._authService.currentRole;
+			this.fullName = user ? `${user.name} ${user.lastname}` : this._authService.currentFullName;
+			this.role = user ? user.role : this._authService.currentRole;
 			this.letter = this.fullName[0];
-    });
+		});
 	}
 
-  toggleSidebar() {
-    this.sidebarToggled.emit();
-  }
+	toggleSidebar() {
+		this.sidebarToggled.emit();
+	}
 
-	toggleUserMenu(){
+	toggleUserMenu() {
 		this.isUserMenu = !this.isUserMenu;
 	}
 }
--- src/app/dashboard/components/dashboard-sidebar/dashboard-sidebar.component.ts
+++ src/app/dashboard/components/dashboard-sidebar/dashboard-sidebar.component.ts
@@ -1,25 +1,52 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth/services/auth.service';
+import { ConfirmationPopupComponent } from 'src/app/shared/components/confirmation-popup/confirmation-popup.component';
 
 @Component({
-  selector: 'app-dashboard-sidebar',
-  templateUrl: './dashboard-sidebar.component.html',
-  styleUrls: ['./dashboard-sidebar.component.scss']
+	selector: 'app-dashboard-sidebar',
+	templateUrl: './dashboard-sidebar.component.html',
+	styleUrls: ['./dashboard-sidebar.component.scss']
 })
 export class DashboardSidebarComponent {
 	@Output() sidebarToggled = new EventEmitter<void>();
-	constructor(private _authService: AuthService){}
+	constructor(
+		private _router: Router,
+		private _dialog: MatDialog,
+		private _authService: AuthService) { }
 
-	showOption(allowedRoles: string[]){
+	showOption(allowedRoles: string[]) {
 		return this._authService.hasRole(allowedRoles);
 	}
 
 	toggleSidebar() {
-    this.sidebarToggled.emit();
-  }
+		this.sidebarToggled.emit();
+	}
 
 
-	logout(){
-		this._authService.logout().subscribe();
+	logout() {
+		const title = 'Cerrar sesión';
+		const subtitle = '¿Seguro de que desea salir del sistema?';
+		const dialogRef = this._dialog.open(ConfirmationPopupComponent, {
+			width: '380px',
+			height: 'auto',
+			autoFocus: false,
+			data: {
+				icon: '../../../../assets/svg/logout_verde.svg',
+				title,
+				subtitle,
+				type: 'edit',
+			},
+		});
+
+		dialogRef.afterClosed().subscribe((result) => {
+			if (result) {
+				this._authService.logout().subscribe(res => {
+					if (res.success) this._router.navigateByUrl('/auth/sign-in');
+				});
+			}
+		});
+
 	}
 }
